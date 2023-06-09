278cabc6c28ce4ec8ab5f3a824ed7ccd0e96c624
Ismail020 <isocetin1907@gmail.com>
2023-06-09 14:23:47 +0200

    Authorization

--- app/Http/Controllers/Api/V2/TaskController.php
+++ app/Http/Controllers/Api/V2/TaskController.php
@@ -10,6 +10,11 @@ use App\Http\Requests\UpdateTaskRequest;
 
 class TaskController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Task::class);
+    }
+
     /**
      * Display a listing of the resource.
      */
